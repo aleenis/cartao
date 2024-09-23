0fa5408533c17da5c9a0ee2d60f02a2b85e7c3c5
aleenis <147100081+aleenis@users.noreply.github.com>
2024-09-23 19:13:52 -0300

    pp

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 <script src="app.js"></script>
 <script src="perguntas.js"></script>
+let respostaEstaVisivel = false
+function viraCartao(){
+    respostaestaVisivel = !respostaEstaVisivel
+}
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
